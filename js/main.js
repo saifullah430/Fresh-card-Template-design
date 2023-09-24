eb6434e929f7f2facf2247c0c1d8494853ed674d
saifullah430 <saifulli517@gmail.com>
2023-09-24 22:11:20 +0600

    complete

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:16,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
